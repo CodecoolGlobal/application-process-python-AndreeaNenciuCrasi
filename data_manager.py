3f627ef6e1d0b1d96b499d5f0b3c3d6fff3219a1
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2019-12-27 18:40:27 +0200

    Recreate project

--- data_manager.py
+++ data_manager.py
@@ -10,3 +10,43 @@ def get_mentor_names_by_first_name(cursor, first_name):
                    {'first_name': first_name})
     names = cursor.fetchall()
     return names
+
+
+@database_common.connection_handler
+def get_mentor_full_names(cursor):
+    cursor.execute("""
+                    SELECT first_name, last_name FROM mentors;
+                   """)
+    names = cursor.fetchall()
+    return names
+
+
+@database_common.connection_handler
+def get_mentor_nicknames(cursor):
+    cursor.execute("""
+                    SELECT nick_name FROM mentors;
+                   """)
+    names = cursor.fetchall()
+    return names
+
+
+@database_common.connection_handler
+def get_applicant_name(cursor, first_name):
+    cursor.execute("""
+                    SELECT CONCAT(first_name, ' ', last_name) AS full_name, phone_number
+                    FROM applicants WHERE first_name = %(first_name)s ORDER BY first_name;
+                   """,
+                   {'first_name': first_name})
+    names = cursor.fetchall()
+    return names
+
+
+@database_common.connection_handler
+def get_applicant_name_by_email(cursor, string):
+    cursor.execute("""
+                    SELECT CONCAT(first_name, ' ', last_name) AS full_name, phone_number
+                    FROM applicants WHERE email LIKE %string;
+                   """,
+                   {'email': string})
+    names = cursor.fetchall()
+    return names
--- main.py
+++ main.py
@@ -16,6 +16,33 @@ def mentor_names():
 
     return render_template('mentor_names.html', mentor_names=mentor_names)
 
+@app.route('/mentors-full-name')
+def mentor_full_names():
+    mentor_full_names = data_manager.get_mentor_full_names()
+
+    return render_template('mentor_names.html', mentor_full_names=mentor_full_names)
+
+
+@app.route('/mentors-nicknames')
+def mentor_nicknames():
+    mentor_nicknames = data_manager.get_mentor_nicknames()
+
+    return render_template('mentor_names.html', mentor_nicknames=mentor_nicknames)
+
+
+@app.route('/applicant-name')
+def applicant_name():
+    applicant_names = data_manager.get_applicant_name('Carol')
+
+    return render_template('mentor_names.html', applicant_names=applicant_names)
+
+
+@app.route('/applicant-name-by-email')
+def applicant_name_by_email():
+    applicant_names_by_email = data_manager.get_applicant_name_by_email('@adipiscingenimmi.edu')
+
+    return render_template('mentor_names.html', applicant_names_by_email=applicant_names_by_email)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
